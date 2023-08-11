01e73a2ae2f414f828519e353e970fd464036fbd
gazservissana.zp@gmail.com <gazservissana.zp@gmail.com>
2023-08-11 20:50:08 +0300

    Initial Commit

---
+++ Quadratic equation/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Quadratic equation
+//
+//  Created by user on 11.08.2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
